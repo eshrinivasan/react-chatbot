377a7342e40171e8aeb303221559876f74291c04
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2019-08-03 12:56:51 +0530

    integrate wikipedia chat bot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,123 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import ChatBot, { Loading } from "react-simple-chatbot";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class DBPedia extends Component {
+  constructor(props) {
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.state = {
+      loading: true,
+      result: "",
+      trigger: false
+    };
+
+    this.triggetNext = this.triggetNext.bind(this);
+  }
+
+  componentWillMount() {
+    const self = this;
+    const { steps } = this.props;
+    const search = steps.search.value;
+    const endpoint = encodeURI("https://dbpedia.org");
+    const query = encodeURI(`
+      select * where {
+      ?x rdfs:label "${search}"@en .
+      ?x rdfs:comment ?comment .
+      FILTER (lang(?comment) = 'en')
+      } LIMIT 100
+    `);
+
+    const queryUrl = `https://dbpedia.org/sparql/?default-graph-uri=${endpoint}&query=${query}&format=json`;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", readyStateChange);
+
+    function readyStateChange() {
+      if (this.readyState === 4) {
+        const data = JSON.parse(this.responseText);
+        const bindings = data.results.bindings;
+        if (bindings && bindings.length > 0) {
+          self.setState({ loading: false, result: bindings[0].comment.value });
+        } else {
+          self.setState({ loading: false, result: "Not found." });
+        }
+      }
+    }
+
+    xhr.open("GET", queryUrl);
+    xhr.send();
+  }
+
+  triggetNext() {
+    this.setState({ trigger: true }, () => {
+      this.props.triggerNextStep();
+    });
+  }
+
+  render() {
+    const { trigger, loading, result } = this.state;
+
+    return (
+      <div className="dbpedia">
+        {loading ? <Loading /> : result}
+        {!loading && (
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: 20
+            }}
+          >
+            {!trigger && (
+              <button onClick={() => this.triggetNext()}>Search Again</button>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+DBPedia.propTypes = {
+  steps: PropTypes.object,
+  triggerNextStep: PropTypes.func
+};
+
+DBPedia.defaultProps = {
+  steps: undefined,
+  triggerNextStep: undefined
+};
+
+// const ExampleDBPedia = () => (
+
+// );
+
+ReactDOM.render(
+  <div>
+    <ChatBot
+      steps={[
+        {
+          id: "1",
+          message: "Type something to search on Wikipédia. (Ex.: Brazil)",
+          trigger: "search"
+        },
+        {
+          id: "search",
+          user: true,
+          trigger: "3"
+        },
+        {
+          id: "3",
+          component: <DBPedia />,
+          waitAction: true,
+          trigger: "1"
+        }
+      ]}
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+//export default ExampleDBPedia;
